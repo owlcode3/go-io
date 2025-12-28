9f7829f3fdb65bbb046745857f57ebc89f59f207
owlcode3 <akoladeo890@yahoo.com>
2025-12-28 09:54:15 +0100

    learn about io.pipe

---
+++ pipe/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	reader, writer := io.Pipe()
+
+	var data = map[string]any{
+		"title":  "foo",
+		"body":   "bar",
+		"userId": 1,
+	}
+
+	go func() {
+		if err := json.NewEncoder(writer).Encode(data); err != nil {
+			writer.CloseWithError(fmt.Errorf("writer failed to write: %w", err))
+			return
+		}
+		writer.Close()
+	}()
+
+	resp, err := http.Post("https://jsonplaceholder.typicode.com/posts", "application/json", reader)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	var bodyData map[string]any
+	if err := json.NewDecoder(resp.Body).Decode(&bodyData); err != nil {
+		panic(err)
+	}
+
+	bodyByte, err := json.MarshalIndent(bodyData, "", " ")
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = fmt.Fprintln(os.Stdout, string(bodyByte))
+	if err != nil {
+		panic(err)
+	}
+}
